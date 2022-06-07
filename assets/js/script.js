10184b47c74d36005f361edd59cda965c680dadf
Andrew Yeh <ayeh2317@gmail.com>
2022-06-06 23:18:16 -0700

    date visible on page, testing click event on buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,41 @@
 let hourContainer = $(`.container`);
+let dateEl = $(`#currentDay`);
+let hoursArray = [9,10,11,12,13,14,15,16,17];
+let currHour = parseInt(moment().format('H'));
 
-let hoursArray = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
-
-function addTimes() {
+function addTimes() {   //adds rows to container
     for(let i=0; i<hoursArray.length; i++) {
-        let rowEl = $(`<div>`).addClass(`row`);
-        let timeEl = $(`<div>`).addClass(`col-1 hour`).text(hoursArray[i]);
+        let rowEl = $(`<div>`).addClass(`row`); //new row
+
+        let time = '';
+        if(hoursArray[i] > 12) {
+            time = `${hoursArray[i]-12}PM`;
+        }
+        else {
+            time = `${hoursArray[i]}AM`;
+        }
+        let timeEl = $(`<div>`).addClass(`col-1 hour`).text(time);  //sets time for each row
+
         let textAreaEl = $(`<textarea>`).addClass(`col-10`);
+        if(hoursArray[i] < currHour) {  //sets background color of textarea based on current hour
+            textAreaEl.addClass(`past`);
+        }
+        else if(hoursArray[i] === currHour) {
+            textAreaEl.addClass(`present`);
+        }
+        else if(hoursArray[i] > currHour) {
+            textAreaEl.addClass(`future`);
+        }
+
         let buttonEl = $(`<button>`).addClass(`col-1 btn save-button saveBtn`);
         let saveIcon = $(`<i>`).addClass(`fas fa-save`);
+
         buttonEl.append(saveIcon);
+        buttonEl.on('click',function() {
+            console.log('BUTTON CLICKED');
+            console.log(hoursArray[i]);
+        });
+
         rowEl.append(timeEl);
         rowEl.append(textAreaEl);
         rowEl.append(buttonEl);
@@ -17,10 +43,18 @@ function addTimes() {
     }
 }
 
-hourContainer.on('click', function(event) {
-    let el = event.element;
-    let parEl = el.closest('div');
-    console.log(parEl);
-});
+function setDate() {
+    dateEl.text(moment().format('dddd, MMMM do'));  //sets date on screen
+}
 
+function saveEvent() {
+
+}
+
+// hourContainer.on('click', function(event) {
+//     let el = event.target;
+//     console.log(el);
+// });
+
+setDate();
 addTimes();
